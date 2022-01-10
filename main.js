1efe963975300c0b575ce10bf0439b7aadfa6ed5
StzaryC <80788755+StzaryC@users.noreply.github.com>
2022-01-10 12:16:43 -0600

    first commit
    commit daily challenge

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function share(){
+    let share_profile_ac = document.querySelector("#share_profile_ac")
+    let share_profile = document.querySelector("#share_profile")
+
+    share_profile.classList.toggle("hidden")
+    share_profile_ac.classList.toggle("hidden")
+}
